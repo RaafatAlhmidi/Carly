dc9ccd3dabd59939707ad4885c6663bbe6d0f302
Raafat Alhmidi <alhoumaidy93@gmail.com>
2024-05-10 01:02:09 +0200

    preparing the dashboardScreen ui

--- app/src/main/java/com/raafat/carly/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/raafat/carly/screens/dashboard/DashboardScreen.kt
@@ -1,22 +1,60 @@
 package com.raafat.carly.screens.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.raafat.carly.Greeting
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.raafat.carly.R
 import com.raafat.carly.ui.theme.CarlyTheme
 
 
 @Composable
-fun DashboardScreen(){
+fun DashboardScreen() {
     CarlyTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .paint(
+                        painter = painterResource(id = R.drawable.dashboard_background),
+                        contentScale = ContentScale.FillBounds
+                    )
+            ) {
+
+                Icon(
+                    modifier = Modifier
+                        .size(130.dp)
+                        .align(Alignment.CenterHorizontally),
+                    painter = painterResource(id = R.drawable.carly_logo),
+                    contentDescription = null
+                )
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_add),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .size(120.dp)
+                            .align(Alignment.Center)
+                    )
+                }
+            }
         }
     }
 }
